8e9cd1a03573984320b0b3ff3a379fc301a120c4
Baudino Sergio <bs105164@etu.unice.fr>
2014-05-22 03:08:03 -0700

    TP4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,12 @@ describe "StaticPages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title', 
-                                :text => "Ruby on Rails Tutorial Example App | Home")
+                                :text => "Ruby on Rails Tutorial Example App")
+    end
+    it "should not have a custom title page" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', 
+                                :text => "| Home")
     end
   end
   describe "Help page" do
